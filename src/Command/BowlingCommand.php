2df35113ae573b016747def62dff0ea19fae13f0
Alexis Sánchez <aasanchez@gmail.com>
2020-11-20 00:38:26 +0100

    psalm fix

--- src/Command/BowlingCommand.php
+++ src/Command/BowlingCommand.php
@@ -9,23 +9,24 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 
-class BowlingCommand extends Command {
+class BowlingCommand extends Command
+{
 
     protected static $defaultName = 'bowling:get-score';
 
     private $frames;
 
-    private $score;
-
     private $rolls = [];
 
     private $points = [0];
 
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct();
     }
 
-    protected function configure(): void {
+    protected function configure(): void
+    {
         $this->setDescription('Start a new Game');
         $this->setHelp('
 A program, which, given a valid sequence of rolls for one line of American 
@@ -34,7 +35,8 @@ Ten-Pin Bowling, produces the total score for the game.');
         $this->addOption('style', null, InputOption::VALUE_OPTIONAL, 'Type of Output', 1);
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output): int {
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
         $scoreLine = $input->getOption('score');
         $this->frames = $this->getFrames($scoreLine);
 
@@ -47,39 +49,50 @@ Ten-Pin Bowling, produces the total score for the game.');
         return Command::SUCCESS;
     }
 
-    protected function getFrames(String $score) {
+    protected function getFrames(string $score)
+    {
         return explode(' ', $score);
     }
 
 
-    protected function getRolls($frame, $throw) {
+    protected function getRolls($frame, $throw)
+    {
         $turn = str_split($frame, 1);
         if ($throw < 9) {
-            if ($turn[0] == 'X' && count($turn) == 1) {
-                array_push($this->rolls, 10, 0);
-            } else if ($turn[1] == '/' && count($turn) == 2) {
-                array_push($this->rolls, intval($turn[0]), 10 - intval($turn[0]));
-            } else if ($turn[1] == '-' && count($turn) == 2) {
-                array_push($this->rolls, intval($turn[0]), 0);
-            } else {
-                array_push($this->rolls, intval($turn[0]), intval($turn[1]));
-            }
+            $this->pairAnalisys($turn);
         } else {
-            foreach ($turn as $shot) {
-                if ($shot == 'X') {
-                    array_push($this->rolls, 10);
-                } else if ($shot == '/') {
-                    array_push($this->rolls, 10 - intval(end($this->rolls)));
-                } else {
-                    array_push($this->rolls, intval($shot));
-                }
-            }
-
+            $this->finalFrame($turn);
         }
     }
 
-    protected function calculatePoints() {
-        $loc = 0;
+    protected function pairAnalisys($turn)
+    {
+        if ($turn[0] == 'X' && count($turn) == 1) {
+            array_push($this->rolls, 10, 0);
+        } elseif ($turn[1] == '/' && count($turn) == 2) {
+            array_push($this->rolls, intval($turn[0]), 10 - intval($turn[0]));
+        } elseif ($turn[1] == '-' && count($turn) == 2) {
+            array_push($this->rolls, intval($turn[0]), 0);
+        } else {
+            array_push($this->rolls, intval($turn[0]), intval($turn[1]));
+        }
+    }
+
+    protected function finalFrame($turn)
+    {
+        foreach ($turn as $shot) {
+            if ($shot == 'X') {
+                array_push($this->rolls, 10);
+            } elseif ($shot == '/') {
+                array_push($this->rolls, 10 - intval(end($this->rolls)));
+            } else {
+                array_push($this->rolls, intval($shot));
+            }
+        }
+    }
+
+    protected function calculatePoints()
+    {
         for ($i = 0; $i < count($this->rolls) - 1; $i += 2) {
             if ($this->rolls[$i] == 10 && $this->rolls[$i + 1] == 0) { //Strike
                 //Need to detect two strikes in a row
@@ -89,7 +102,7 @@ Ten-Pin Bowling, produces the total score for the game.');
                     $value = $this->rolls[$i + 2] + $this->rolls[$i + 3];
                 }
                 $this->points[] = end($this->points) + 10 + $value;
-            } else if (($this->rolls[$i] + $this->rolls[$i + 1]) == 10 && $this->rolls[$i + 1] != 0) { //Spare
+            } elseif (($this->rolls[$i] + $this->rolls[$i + 1]) == 10 && $this->rolls[$i + 1] != 0) { //Spare
                 $this->points[] = end($this->points) + 10 + $this->rolls[$i + 2];
             } else { // Open Frame
                 $this->points[] = end($this->points) + $this->rolls[$i] + $this->rolls[$i + 1];
